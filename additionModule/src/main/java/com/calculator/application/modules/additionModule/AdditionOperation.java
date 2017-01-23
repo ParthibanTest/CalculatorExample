f7efddb67cbbc719ab74b689633ed122d6b5806b
PKMT206 <PKMT206@MGTECHWKS14.MGTECHSOFT.IN>
2017-01-23 16:56:40 +0530

    change the logic

--- additionModule/src/main/java/com/calculator/application/modules/additionModule/AdditionOperation.java
+++ additionModule/src/main/java/com/calculator/application/modules/additionModule/AdditionOperation.java
@@ -2,6 +2,6 @@ package com.calculator.application.modules.additionModule;
 
 public class AdditionOperation {
 	public double addition(double temp, double value) {
-		return temp += value;
+		return temp -= value;
 	}
 }
