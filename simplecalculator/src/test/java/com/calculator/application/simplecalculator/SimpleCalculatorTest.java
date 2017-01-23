f94fa1a79e2144afd6a9fdb57ee5abf3fac3b92b
PKMT206 <PKMT206@MGTECHWKS14.MGTECHSOFT.IN>
2017-01-23 15:11:02 +0530

    Modified the test cases.

--- simplecalculator/src/test/java/com/calculator/application/simplecalculator/SimpleCalculatorTest.java
+++ simplecalculator/src/test/java/com/calculator/application/simplecalculator/SimpleCalculatorTest.java
@@ -1,16 +1,16 @@
 package com.calculator.application.simplecalculator;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 import com.calculator.application.modules.DivisionModule.DivisionOperation;
 import com.calculator.application.modules.MultiplicationModule.MultiplicationOperation;
 import com.calculator.application.modules.additionModule.AdditionOperation;
 import com.subtraction.operation.SubtractionOperation;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
 
 public class SimpleCalculatorTest 
-    extends TestCase
 {
 	// Mock Data
 	double a=5;
@@ -27,10 +27,8 @@ public class SimpleCalculatorTest
      *
      * @param testName name of the test case
      */
-    public SimpleCalculatorTest( String testName )
+    public SimpleCalculatorTest()
     {
-        super( testName );
-        
     	//Initialize the calculator operational Modules
 		additionOperation = new AdditionOperation();
 		subtractionOperation = new SubtractionOperation();
@@ -38,20 +36,13 @@ public class SimpleCalculatorTest
 		divisionOperation = new DivisionOperation();
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( SimpleCalculatorTest.class );
-    }
-
+    @Test
     public void testApp()
     {
-        assertEquals( additionOperation.addition(a, b), 15.0);
-        assertEquals( subtractionOperation.subtract(a, b), 5.0);
-        assertEquals( multiplicationOperation.multiple(a, b), 50.0);
-        assertEquals( divisionOperation.divide(a, b), 2.0);
+        assertEquals( additionOperation.addition(a, b), 15.0, 0.001);
+        assertEquals( subtractionOperation.subtract(a, b), 5.0, 0.001);
+        assertEquals( multiplicationOperation.multiple(a, b), 50.0, 0.001);
+        assertEquals( divisionOperation.divide(a, b), 2.0, 0.001);
 
     }
 }
